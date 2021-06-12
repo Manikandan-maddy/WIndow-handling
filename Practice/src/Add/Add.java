6d01eb186b3df67e849b018004799030910a9ef1
Maddy <Maddy@192.168.0.106>
2021-06-12 16:09:08 +0530

    Esclipe import

---
+++ Practice/src/Add/Add.java
@@ -0,0 +1,20 @@
+package Add;
+
+public class Add {
+	int a,b,c;
+	
+	public void addfunction()
+	{
+	int a=10;
+	b=20;
+	c=a+b;
+	System.out.println("The value result is="+c);
+	}
+	public static void main(String[]args)
+	{
+		
+		Add r = new Add ();
+		r.addfunction();
+	}
+	
+}
---
+++ Practice/src/Add/circle.java
@@ -0,0 +1,54 @@
+package Add;
+import java.util.*;
+
+public class circle {
+
+	
+
+
+public static void main(String[]args)
+{
+	int cir;
+	int rec;
+	int tri;
+    String shape;
+ 
+	Scanner a = new Scanner(System.in);
+	
+	System.out.println("Enter the radius of the Circle=");
+	cir=a.nextInt();
+	System.out.println("your radius of the Circle="+cir);
+	
+	System.out.println("Enter the length of the rectangle=");
+	rec=a.nextInt();
+	System.out.println("Enter the length of the rectangle="+rec);
+	
+	System.out.println("Enter the Shape of the triangle=");
+    tri=a.nextInt();
+    System.out.println("Enter the Shape of the triangle="+tri);
+	
+	System.out.println("Now your Shape is");
+
+	System.out.println("Name of the Shape is=");
+	shape =a.next();
+	System.out.println("Your the Shape of the Diagram is="+shape);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
